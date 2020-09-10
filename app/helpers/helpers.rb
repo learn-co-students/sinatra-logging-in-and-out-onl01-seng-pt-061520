fa3be4af515a2a29a81c0742af72add9aeaa59fd
IgoVeyner <igorveyner95@gmail.com>
2020-09-10 11:14:21 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,10 @@
+# Class creates helper methods to validate if correct user is logged in
 class Helpers
+  def self.current_user(session = {:user_id => nil})
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session = {:user_id => nil})
+    session[:user_id] ? true : false
+  end
 end
