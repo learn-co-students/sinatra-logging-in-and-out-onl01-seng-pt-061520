b177972f25e76ea4ac72da2f2bbcbd09d1ffbdf1
hopegipson <gipson.hope@gmail.com>
2020-09-17 18:07:45 -0600

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+    def self.current_user(session = {:user_id => nil})
+    User.find(session[:user_id])
+    end
+
+    def self.is_logged_in?(session = {:user_id => nil})
+        !!session[:user_id]
+    end
 end
